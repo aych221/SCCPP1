25c24f6c4e6df59062322aa62bf551a66857ba1b
wanghochiu <wchiu@augusta.edu>
2023-03-08 12:21:32 -0500

    binded CreateMain html to cs

--- Pages/CreateMainProfile.cshtml.cs
+++ Pages/CreateMainProfile.cshtml.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using SCCPP1.Session;
 
 namespace SCCPP1.Pages
 {
+    [BindProperties]
     public class CreateMainProfileModel : SessionModel
     {
         private readonly ILogger<PrivacyModel> _logger;
@@ -15,6 +17,40 @@ namespace SCCPP1.Pages
         public IActionResult OnGet()
         {
             return Page();
+        }
+
+        [BindProperty]
+        public Person person { get; set; }
+
+        public void OnPost()
+        {
+            ViewData["UserData"] = "User Information: "
+                + person.firstname + " " + person.middlename + " " + person.lastname
+                + person.phonenumber
+                + person.eaddress
+                + person.about
+                + person.experience
+                + person.complanguages
+                + person.operatingsystems
+                + person.softandframe
+                + person.educationcertification
+                + person.educationyear;
+        }
+
+        public class Person
+        {
+            public string firstname { get; set; }
+            public string lastname { get; set; }
+            public string middlename { get; set; }
+            public string phonenumber { get; set; }
+            public string eaddress { get; set; }
+            public string about { get; set; }
+            public string experience { get; set; }
+            public string complanguages { get; set; }
+            public string operatingsystems { get; set; }
+            public string softandframe { get; set; }
+            public string educationcertification { get; set; }
+            public string educationyear { get; set; }
         }
     }
 }
